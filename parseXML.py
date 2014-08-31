9be1bec1246c6222ae003bfd6b8a9c358ea5e5c6
ghtub <vik.reddy@gmail.com>
2014-08-31 08:43:58 -0700

    updated parsing

--- parseXML.py
+++ parseXML.py
@@ -3,10 +3,26 @@ import xml.etree.ElementTree as ET
 class ParseXML:
     """A simple example class for parsing XML"""
     i = 12345
+    def printChildren(self,root):
+		for child in root:
+			print child.tag, child.attrib
+	def printGrandChildren(self,child):
+		for grandchild in child:
+			print grandchild.tag, grandchild.attrib
     def __init__(self,filename):
         self.tree = ET.parse(filename)
         self.root = self.tree.getroot()
-        
-parser = ParseXML("sample.xml")
+	
 
-print parser.root.tag
+			
+parser2 = ParseXML("sample.xml")
+parser = ParseXML("iTunes Music Library.xml")
+
+print parser2.root.tag
+root = parser2.root
+parser2.printChildren(root)
+    
+#print parser.root.tag
+#parser.printGrandChildren(parser.root.getchildren())
+
+# for reference: https://docs.python.org/2/library/xml.etree.elementtree.html
